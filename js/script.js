cdf962a0b7243384b6f39bf876d61f47409d0837
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-05-17 00:59:19 +0900

    .
    .

--- js/script.js
+++ js/script.js
@@ -10,4 +10,31 @@ window.onload = function() {
         gnbMenu.classList.toggle("active");
     };
 
+    // ---------- scrollTop -------------------
+    const scrollTop = document.querySelector("#scrollTop");
+    scrollTop.onclick = function() {
+        window.scrollTo({
+            top : 0,
+            left : 0,
+            behavior : 'smooth'
+        });
+    }
+
+
+    
 }
+// ---------- login --------------
+
+// $("#loginForm").validate({
+//     rules: {
+//         username: { required: true },
+//         password: { required: true }
+//     },
+//     messages: {
+//         username: { required: "아이디를 입력하세요." },
+//         password: { required: "비밀번호를 입력하세요." }
+//     }
+// });
+
+// let returnCode = document.login.returnCode.value;
+// let access = document.login.access.value;
