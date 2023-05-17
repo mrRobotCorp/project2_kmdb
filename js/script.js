cdce9e1dae080411c1213e27a6ed01efb87831d2
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-05-18 00:33:02 +0900

    .
    .

--- js/script.js
+++ js/script.js
@@ -43,6 +43,9 @@ $(document).ready(function(){
     $('.slider').slick({
         dots: true, 
         arrow: true,
+        infinite: true, 
+        autoplay: true,
+        autoplaySpeed : 1800,
         slidesToShow: 1,
         slidesToScroll: 1, 
     });
