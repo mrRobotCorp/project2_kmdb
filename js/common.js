9c412bba359ab9b3afd1a230ac888d3c61ef4d38
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-05-19 02:24:32 +0900

    .
    .

--- js/common.js
+++ js/common.js
@@ -20,6 +20,20 @@ window.onload = function() {
         });
     }
 
+    // --------------- register ---------------
+    const rg = document.querySelector(".toRegister");
+    let rgAnsw;
+
+    rg.onclick = function() {
+        rgAnsw = confirm("한국영상자료원 회원이 되시면 하나의 통합된 회원 ID와 비밀번호로 \n한국영상자료원과 KMDb의 서비스를 모두 이용하실 수 있습니다.\n통합 회원가입 하시겠습니까?");
+        console.log(rgAnsw);
+        if(rgAnsw == true) {
+            window.location.replace('https://www.koreafilm.or.kr/member/main');
+        } else {
+            window.location.replace('register.html');
+        }
+    }
+
 
     
 }
